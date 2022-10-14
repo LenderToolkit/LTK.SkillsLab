36b50645405436b31431f92951dd4f83a12fe598
Prodev <tomasjin1024@gmail.com>
2022-10-14 03:01:52 -0400

    update: updated with btn style

--- skills-test-react/src/components/Header/index.js
+++ skills-test-react/src/components/Header/index.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {
-  Avatar, 
-  Box, 
-  IconButton, 
-  Menu, 
-  MenuItem, 
-  Typography, 
-  Tooltip
-} from '@mui/material';
+  Avatar,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Typography,
+  Tooltip,
+} from "@mui/material";
 
-import { PATH, CONSTANTS } from '../../constants';
-import { Button } from '@mui/material';
+import { PATH, CONSTANTS } from "../../constants";
+import { Button } from "@mui/material";
 
 export default function HeaderComponent({ onClick }) {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -24,23 +24,36 @@ export default function HeaderComponent({ onClick }) {
     setAnchorEl(null);
   };
   const handleTodopage = () => {
-    navigate(PATH.TODO)
-  }
+    navigate(PATH.TODO);
+  };
   return (
     <>
-      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center', my: '20px' }}>
-        <Typography sx={{ minWidth: 100 }}>{CONSTANTS.general.contact}</Typography>
-        <Typography sx={{ minWidth: 100 }}>{CONSTANTS.general.profile}</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          textAlign: "center",
+          my: "20px",
+        }}
+      >
+        <Typography sx={{ minWidth: 100 }}>
+          {CONSTANTS.general.contact}
+        </Typography>
+        <Typography sx={{ minWidth: 100 }}>
+          {CONSTANTS.general.profile}
+        </Typography>
         <Tooltip title="Account settings">
           <IconButton
             onClick={handleClick}
             size="small"
             sx={{ ml: 2 }}
-            aria-controls={open ? 'account-menu' : undefined}
+            aria-controls={open ? "account-menu" : undefined}
             aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
+            aria-expanded={open ? "true" : undefined}
           >
-            <Avatar sx={{ width: 32, height: 32 }}>{CONSTANTS.general.m}</Avatar>
+            <Avatar sx={{ width: 32, height: 32 }}>
+              {CONSTANTS.general.m}
+            </Avatar>
           </IconButton>
         </Tooltip>
       </Box>
@@ -53,34 +66,42 @@ export default function HeaderComponent({ onClick }) {
         PaperProps={{
           elevation: 0,
           sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
             mt: 1.5,
-            '& .MuiAvatar-root': {
+            "& .MuiAvatar-root": {
               width: 32,
               height: 32,
               ml: -0.5,
               mr: 1,
             },
-            '&:before': {
+            "&:before": {
               content: '""',
-              display: 'block',
-              position: 'absolute',
+              display: "block",
+              position: "absolute",
               top: 0,
               right: 14,
               width: 10,
               height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
               zIndex: 0,
             },
           },
         }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
-        <MenuItem onClick={onClick}>
-            <Button startIcon={<Avatar />} variant="text" onClick={handleTodopage}> {CONSTANTS.general.openForm}</Button>            
+        <MenuItem onClick={onClick} sx={{ p: "0" }}>
+          <Button
+            fullWidth
+            startIcon={<Avatar />}
+            variant="text"
+            onClick={handleTodopage}
+            sx={{ px: "16px", py: "9px" }}
+          >
+            {CONSTANTS.general.openForm}
+          </Button>
         </MenuItem>
       </Menu>
     </>
