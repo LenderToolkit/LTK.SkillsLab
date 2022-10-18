f212af486e019d9d7a334f94b5f9cd30b12b5dfb
Prodev <tomasjin1024@gmail.com>
2022-10-18 08:15:59 -0400

    update: updated with elipse for table

--- skills-test-react/src/components/TodoTable/index.js
+++ skills-test-react/src/components/TodoTable/index.js
@@ -1,10 +1,10 @@
-import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useCallback } from "react";
+import { useDispatch } from "react-redux";
 
-import { Actions } from '../../store';
+import { Actions } from "../../store";
 
-import { CONSTANTS } from '../../constants';
-import { Delete as DeleteIcon } from '@mui/icons-material';
+import { CONSTANTS } from "../../constants";
+import { Delete as DeleteIcon } from "@mui/icons-material";
 
 import {
   Button,
@@ -13,8 +13,8 @@ import {
   TableBody,
   TableHead,
   TableRow,
-  TableCell
-} from '@mui/material';
+  TableCell,
+} from "@mui/material";
 
 const TodoTableComponent = ({ todos }) => {
   const dispatch = useDispatch();
@@ -24,39 +24,39 @@ const TodoTableComponent = ({ todos }) => {
   });
 
   return (
-    <Paper sx={{my: '20px'}}>
+    <Paper sx={{ my: "20px" }}>
       <Table>
         <TableHead>
           <TableRow>
             <TableCell>{CONSTANTS.tableHeader.number}</TableCell>
             <TableCell align="center">{CONSTANTS.tableHeader.name}</TableCell>
-            <TableCell align="right">{CONSTANTS.tableHeader.action}</TableCell>
+            <TableCell align="center">{CONSTANTS.tableHeader.action}</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {
-            todos.map((todo, index) =>
-              <TableRow key={index}>
-                <TableCell>{index + 1}</TableCell>
-                <TableCell>{todo}</TableCell>
-                <TableCell  align="right">
-                  <Button 
-                  variant="outlined" 
-                  startIcon={<DeleteIcon />} 
+          {todos.map((todo, index) => (
+            <TableRow key={index}>
+              <TableCell sx={{width: "15%"}}>{index + 1}</TableCell>
+              <TableCell align="center">
+                <div style={{width: "300px", overflow: "hidden", textOverflow: "ellipsis"}}>{todo}</div>
+              </TableCell>
+              <TableCell align="center"  sx={{width: "25%"}}>
+                <Button
+                  variant="outlined"
+                  startIcon={<DeleteIcon />}
                   onClick={() => onDeleteTodo(index)}
                   size="small"
                   color="error"
-                  >
-                    {CONSTANTS.general.delete}
-                  </Button>
-                </TableCell>            
-              </TableRow>
-            )
-          }
+                >
+                  {CONSTANTS.general.delete}
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </Paper>
-  )
+  );
 };
 
 export default TodoTableComponent;
