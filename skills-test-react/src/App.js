6d77ab23ca2e367efdc8c73b86b57ee3f8d0ff97
tat <tat@SpicyCoconuts.local>
2023-09-28 11:38:00 -0400

    Create Solutions to React Assessment, tat, react_solutions, 57, 20230928_1137

--- skills-test-react/src/App.js
+++ skills-test-react/src/App.js
@@ -1,30 +1,17 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import AccountMenu from './AccountMenu';
-import BasicForm from './BasicForm'
 
-export default function App() {
-	const [openForm, setOpenForm] = React.useState(false);
-	const handleOnClick = (page) => {
-		console.log('open');
-		if(!openForm) { 
-			setOpenForm(true)
-		} else {
-			setOpenForm(false)
-		}
-	};
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomeComponent from './components/HomeComponent';
+import AboutComponent from './components/AboutComponent';
+import logger from './logger';
+function App() {
+  logger.info('App component loaded');
   return (
-    <Container maxWidth="sm">
-	<AccountMenu onClick={handleOnClick} />
-	{openForm && <Basicorm />}
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          React Skills test
-        </Typography>
-      </Box>
-    </Container>
+    <Router>
+      <Switch>
+        <Route path='/home' component={HomeComponent} />
+        <Route path='/about' component={AboutComponent} />
+      </Switch>
+    </Router>
   );
 }
