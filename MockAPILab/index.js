b7110c727b4e1873b7ad9b484ea0a02598fc3f1a
rubenponce <rubenmartinponce@gmail.com>
2023-10-04 15:35:00 -0400

    initial commit

--- MockAPILab/index.js
+++ MockAPILab/index.js
@@ -1,27 +1,56 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8082;
 const isLocal = true;
+const loanObjects = require('./loanObjects.json');
+const router = express.Router();
+const fs = require('fs');
+const dataPath = './loanObjects.json';
 
+app.use(express.json())
+router.use(express.json());
+
+const loanData = []
+const saveLoanData = (loanData) => {
+//	fs.writeFileSync(dataPath, JSON.stringify(loanData));
+    loanData.push(loanData)
+}
+
+const getLoanData = () => {
+    return loanData
+}
 app.get('/', (req, res) => {
-    res.json({
-		message: "✨ 👋🌏 ✨",
-		stage: process.env.NODE_ENV,
-	});
+    const loanData = getLoanData();
+
+    res.json(loanData);
 });
 
 app.get("/ping", (req, res) => {
-	res.json({
-		message: "🏓",
-	});
+    res.json({
+        message: "🏓",
+    });
 });
 
+const isValidLoan = (loan) => {
+    return typeof loan === "object" && loan !== null && loan.borrowers.length >= 0;
+}
+app.post("/addLoan", (req, res) => {
+    console.log({reqBody: req.body,})
+    const existingLoans = getLoanData();
+    const newLoanId = Math.floor(100000 + Math.random() * 900000)
+    req.body.id = newLoanId;
+    existingLoans[newLoanId] = req.body
+
+    saveLoanData(existingLoans);
+    res.send({success: true, msg: 'account added successfully'})
+
+});
 if (isLocal) {
-	//local host
-	app.listen(port, () => {
-		console.log(`Example app listening on port ${port}`)
-	});
+    //local host
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    });
 } else {
-	//for lambda export
-	module.exports = app;
+    //for lambda export
+    module.exports = app;
 }
