e3c1c504ce349852b7c8cf01ec8a6a28246f05d0
rubenponce <rubenmartinponce@gmail.com>
2023-10-04 15:57:42 -0400

    fix build errors

--- skills-test-react/src/AccountMenu.js
+++ skills-test-react/src/AccountMenu.js
@@ -3,15 +3,9 @@ import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Tooltip from '@mui/material/Tooltip';
-import PersonAdd from '@mui/icons-material/PersonAdd';
-import Settings from '@mui/icons-material/Settings';
-import Logout from '@mui/icons-material/Logout';
-import {Link, Outlet} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 export default function AccountMenu({ onClick, openForm }) {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
--- skills-test-react/src/App.js
+++ skills-test-react/src/App.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
 import AccountMenu from './AccountMenu';
 import TodoList from './TodoList'
 import {Route, Routes} from "react-router-dom";
